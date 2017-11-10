ede4dbe6ebc8e17c83ba0cb1946954fbdb8cd93f
Jasper N. Brouwer <jasper@nerdsweide.nl>
2017-11-10 15:49:36 +0100

    Rename tests so the are consistent with the classes they test again

--- tests/EquatableVectorTest.php
+++ tests/EquatableVectorTest.php
@@ -17,7 +17,7 @@ use stdClass;
  * @copyright Copyright (c) 2015 Future500 B.V.
  * @author    Jasper N. Brouwer <jasper@future500.nl>
  */
-final class ImmutableEquatableVectorTest extends TestCase
+final class EquatableVectorTest extends TestCase
 {
     /**
      * @test
